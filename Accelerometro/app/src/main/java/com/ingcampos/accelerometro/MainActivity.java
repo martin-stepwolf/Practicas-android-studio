f108d374aa8271e15312d21c717c73c97ed6d097
IngCampos <15151130@mail.ita.mx>
2019-10-21 10:17:14 -0500

    Accelerometro creacion

---
+++ Accelerometro/app/src/main/java/com/ingcampos/accelerometro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ingcampos.accelerometro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
